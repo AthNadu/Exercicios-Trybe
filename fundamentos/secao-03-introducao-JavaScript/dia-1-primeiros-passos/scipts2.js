219be06d0ec4788c3e11b924e9ee02667cd5009f
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-23 15:49:59 -0300

    Exercício 2

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scipts2.js
@@ -0,0 +1,12 @@
+const base = 5
+
+const heigth = 8
+
+const area = base*heigth
+
+const perimeter = base+base+heigth+heigth
+
+console.log(base)
+console.log(heigth)
+console.log(area)
+console.log(perimeter)
