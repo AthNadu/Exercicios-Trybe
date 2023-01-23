782aed98e913fa1eab324778e24e0321000166fc
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-23 19:38:09 -0300

    Exercício 5 concluído

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts5.js
@@ -0,0 +1,13 @@
+const weekDay = 'Sexta-feira'
+
+let message;
+
+if (weekDay === 'Quarta-feira' || weekDay === 'Segunda-feira' || weekDay === 'Terça-feira' || weekDay === 'Quinta-feira' || weekDay === 'Sexta-feira') {
+    message = 'Oba, mais um dia de aprendizado na Trybe >:D'
+}
+
+else {
+    message = 'FINALMENTE, descanso merecido UwU'
+}
+
+console.log (message)
