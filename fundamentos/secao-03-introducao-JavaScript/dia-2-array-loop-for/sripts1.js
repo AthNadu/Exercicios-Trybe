f58ecd80eca3b56ebd0cbb29094d2527c5d5f852
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-24 19:46:56 -0300

    utilizando o for

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-2-array-loop-for/sripts1.js
@@ -0,0 +1,8 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+console.log ('Precisa de : ');
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    let message = groceryList[index];
+    console.log (message);
+}
