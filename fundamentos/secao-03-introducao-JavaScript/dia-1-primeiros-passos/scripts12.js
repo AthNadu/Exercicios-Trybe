b825385e607f48bfaa08e72aad4c2dd50d474c20
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-25 16:05:11 -0300

    tutorial de xadrez concluído

--- fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts12.js
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts12.js
@@ -1,7 +1,31 @@
-let movimentoXadrez;
+let movimentoXadrez = 'rei';
 
-switch (movimentoXadrez) {
-    case 'peao'
+switch (movimentoXadrez.toLowerCase()) {
+    case 'peao':
+        console.log('Peão : somente pra frente, 1 ou 2 casas')
+        break;
+    
+    case 'torre':
+        console.log('Torre : frente ou lado, quantas casas quiser')
+        break;
+    
+    case 'cavalo':
+        console.log('Cavalo : movimento de L, 2 por 3')
+        break;
 
-    case 
+    case 'bispo':
+        console.log('Bispo : diagonal, quantas quiser')
+        break;
+
+    case 'rainha':
+        console.log('Rainha : qualquer direção, quantas casas quiser')
+        break;
+    
+    case 'rei':
+        console.log('Rei : qualquer direção, apenas uma casa')
+        break;
+
+    default:
+        console.log('Erro, peça inexistente')
+        break;
 }
