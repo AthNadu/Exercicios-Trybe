6d9ad53cc0fca620469221f24b67c96214dd9b96
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-25 00:17:49 -0300

    segundo if completo

--- fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts8.js
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts8.js
@@ -2,10 +2,10 @@ let num1 = 72;
 let num2 = 54;
 
 if (num1 > num2) {
-    console.log ('Acertou, parabéns!!')
+    console.log ('Acertou, A é maior que B parabéns!!')
 }
 else if (num1 < num2) {
-    console.log ('Tente novamente!!')
+    console.log ('Tente novamente, B é maior que A!!')
 }
 else {
     console.log ('não compreendo seu resultado')
---
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts9.js
@@ -0,0 +1,13 @@
+const num1 = 90;
+const num2 = 52;
+const num3 = 44;
+
+if (num1 > num2 && num1 > num3) {
+    console.log('Parabéns o número um é o maior de todos');
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log('Parabén o número 2 é o maior');
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log('O terceiro número é o maior');
+}
