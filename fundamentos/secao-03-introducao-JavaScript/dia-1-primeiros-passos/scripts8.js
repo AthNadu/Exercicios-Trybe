004ff9eb49b3f1b2317ce6b8999a5bb137494170
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-25 00:06:31 -0300

    primeiro if

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts8.js
@@ -0,0 +1,12 @@
+let num1 = 72;
+let num2 = 54;
+
+if (num1 > num2) {
+    console.log ('Acertou, parabéns!!')
+}
+else if (num1 < num2) {
+    console.log ('Tente novamente!!')
+}
+else {
+    console.log ('não compreendo seu resultado')
+}
