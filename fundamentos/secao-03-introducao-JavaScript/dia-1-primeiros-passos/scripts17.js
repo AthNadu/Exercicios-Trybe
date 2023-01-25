0516972610e060a0db4c8bc7da348eb37d47107e
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-25 19:57:24 -0300

    Imposto de renda e INSS feito

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-1-primeiros-passos/scripts17.js
@@ -0,0 +1,37 @@
+let salarioBruto = 6000.00;
+
+let INSS;
+let IR;
+
+if (salarioBruto <= 1556.94) {
+    INSS = salarioBruto * 0.08;
+}
+else if (salarioBruto <= 2594.92) {
+    INSS = salarioBruto * 0.09;
+}
+else if (salarioBruto <= 5189.82) {
+    INSS = salarioBruto * 0.11;
+}
+else {
+    INSS = 570.88;
+}
+
+let salarioLiquido = salarioBruto - INSS;
+
+if (salarioLiquido <= 1903.98) {
+    IR = salarioLiquido - 0;
+}
+else if (salarioLiquido <= 2826.65) {
+    IR = (salarioLiquido * 0.075) - 142.80;
+}
+else if (salarioLiquido <= 3751.05) {
+    IR = (salarioLiquido * 0.15) - 354.80;
+}
+else if (salarioLiquido <= 4664.68) {
+    IR = (salarioLiquido * 0.225) - 636.13;
+}
+else {
+    IR = (salarioLiquido * 0.275) - 869.36;
+};
+
+console.log(salarioLiquido - IR);
