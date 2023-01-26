b314b5dead1bf443415b35442a7bd96a34fe8179
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-25 21:34:52 -0300

    percorrendo e somando arrays

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-2-array-loop-for/scripts3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (indexNumbers = 0; indexNumbers < numbers.length ; indexNumbers += 1) {
+    soma += numbers[indexNumbers];
+}
+
+console.log(soma);
