4c92fdb249104e99a1a9f3711135e478d2049515
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-25 22:20:19 -0300

    adicionando o menor número

--- fundamentos/secao-03-introducao-JavaScript/dia-2-array-loop-for/scripts7.js
+++ fundamentos/secao-03-introducao-JavaScript/dia-2-array-loop-for/scripts7.js
@@ -1,6 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let numeroMaior = 0;
+let numeroMaior =numbers[0];
+
+let numeroMenor =numbers[0];
 
 for (indexNumbers = 1; indexNumbers < numbers.length ; indexNumbers += 1) {
     if (numbers[indexNumbers] > numeroMaior) {
@@ -8,4 +10,12 @@ for (indexNumbers = 1; indexNumbers < numbers.length ; indexNumbers += 1) {
     }
 }
 
-console.log(numeroMaior);
+for (indexNumbers = 1; indexNumbers < numbers.length ; indexNumbers += 1) {
+    if (numbers[indexNumbers] < numeroMenor) {
+        numeroMenor = numbers[indexNumbers];
+    }
+}
+
+console.log('O número maior é:', numeroMaior);
+
+console.log('O número menor é:', numeroMenor);
