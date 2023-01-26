6fc24783b8054cf26a789b5d5a47e8ff9a39ce40
Arthur Nadu <arthurnadu98@gmail.com>
2023-01-26 19:01:06 -0300

    Criando operações matemáticas

---
+++ fundamentos/secao-03-introducao-JavaScript/dia-4-objetos-funcoes/functions1.js
@@ -0,0 +1,25 @@
+function soma(numA,numB) {
+    return numA + numB
+}
+
+function subtracao(numA,numB) {
+    return numA - numB
+}
+
+function multiplicacao(numA,numB) {
+    return numA * numB
+}
+
+function divisao(numA,numB) {
+    return numA / numB
+}
+
+function modulo(numA,numB) {
+    return numA % numB
+}
+
+console.log(soma(20,20));
+console.log(subtracao(20,20));
+console.log(multiplicacao(20,20));
+console.log(divisao(20,20));
+console.log(modulo(20,20));
