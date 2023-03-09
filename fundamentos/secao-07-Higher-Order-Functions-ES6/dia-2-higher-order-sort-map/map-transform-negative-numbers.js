8253f3082f1a50d7c22853a750c819274142e272
Arthur Nadu <arthurnadu98@gmail.com>
2023-03-09 18:30:49 -0300

    Exemplos secao 7 dia 2

---
+++ fundamentos/secao-07-Higher-Order-Functions-ES6/dia-2-higher-order-sort-map/map-transform-negative-numbers.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+// Com for
+// const negativeNumbers = [];
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > 0) {
+//     negativeNumbers.push(numbers[index] * -1);
+//   } else {
+//     negativeNumbers.push(numbers[index]);
+//   }
+// }
